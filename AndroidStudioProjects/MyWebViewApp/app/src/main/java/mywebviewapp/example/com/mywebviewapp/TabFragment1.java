c58ccb63cae57a85ecabb10262565a8da7d716e7
Alicia Guerra <alicia.developer@aliciaguerra.com>
2017-08-24 11:33:23 -0700

    webview redirect browser

--- AndroidStudioProjects/MyWebViewApp/app/src/main/java/mywebviewapp/example/com/mywebviewapp/TabFragment1.java
+++ AndroidStudioProjects/MyWebViewApp/app/src/main/java/mywebviewapp/example/com/mywebviewapp/TabFragment1.java
@@ -6,12 +6,32 @@ package mywebviewapp.example.com.mywebviewapp;
         import android.support.v4.app.Fragment;
         import android.view.LayoutInflater;
         import android.view.View;
+        import android.webkit.WebView;
         import android.view.ViewGroup;
+
 
 public class TabFragment1 extends Fragment {
 
+    WebView myWebView;
+    final static String products = "http://pushauto.com/products/";
+    String myUrl;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+        View view = inflater.inflate(R.layout.tab_fragment_1, container, false);
+        myWebView = (WebView)view.findViewById(R.id.mywebview);
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        if(myUrl==null){
+            myUrl = products;
+        }
+
+        myWebView.loadUrl(myUrl);
+        return view;
+
     }
+
+
+
 }
